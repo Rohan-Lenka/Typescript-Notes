d6eda3a3c1552d7927c18ae83e86a170c3df8eec
Rohan Lenka <rohanlenka999@gmail.com>
2025-03-11 08:26:37 +0530

    started...

---
+++ intro.ts
@@ -0,0 +1,3 @@
+// Typescript(ts) -> js with types
+// Under the hood it basically ts code gets transpiled into js code
+    // transpile -> conv code from one high lvl lang to another high lvl lang with same lvl of abstraction 
